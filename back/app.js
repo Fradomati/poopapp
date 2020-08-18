7eebc255b34556bbe7ab41ccc8ec93c6875ae3ad
Borja Domínguez <bdominguez@MacBook-Air-de-Borja.local>
2020-08-18 17:35:38 +0200

    test deploy from back with only unique application

--- back/app.js
+++ back/app.js
@@ -74,7 +74,7 @@ require("./passport/")(app);
 // app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "hbs");
 
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.static(path.resolve(__dirname, "../client/build")))
 
