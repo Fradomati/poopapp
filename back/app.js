e2c362082c0c3d83493393a81e1638f9cd2d0c96
Borja Domínguez <bdominguez@MacBook-Air-de-Borja.local>
2020-08-20 16:45:05 +0200

    Routes fixed

--- back/app.js
+++ back/app.js
@@ -87,11 +87,11 @@ app.use('/', index);
 
 // Login and Signup Route
 const auth = require("./routes/auth");
-app.use("/api/auth", auth);
+app.use("/auth", auth);
 
 // Data Timer 
 
 const dataTime = require("./routes/dataTime");
-app.use("/api/dataTime", dataTime)
+app.use("/dataTime", dataTime)
 
 module.exports = app;
--- client/src/services/Connections.js
+++ client/src/services/Connections.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 
 export const authService = axios.create({
-    baseURL: `${process.env.BACKEND_URL}/api/auth`,
+    baseURL: `${process.env.BACKEND_URL}/auth`,
     withCredentials: true
 })
 
 export const dataService = axios.create({
-    baseURL: `${process.env.BACKEND_URL}/api/dataTime`,
+    baseURL: `${process.env.BACKEND_URL}/dataTime`,
     withCredentials: true
 })
