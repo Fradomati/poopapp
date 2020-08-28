40f8dd8b50f0ab9eef428efcfe23d2f76166ab2b
Borja Domínguez <borja.d.atienza@gmail.com>
2020-08-28 20:52:37 +0200

    Look online users

--- back/routes/onlineUser.js
+++ back/routes/onlineUser.js
@@ -6,6 +6,8 @@ const _ = require("lodash");
 router.post("/on_user", async (req, res) => {
     const { id } = req.body;
 
+    console.log("id", id)
+
     await OnlineUser.findOneAndUpdate(
         {
             $push: {
