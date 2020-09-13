f6a8dd4bdfcf23217d0e593439e6253d119ce4c5
Borja Domínguez <borja.d.atienza@gmail.com>
2020-09-13 13:06:32 +0200

    Fix error load component without user

--- client/src/services/Connections.js
+++ client/src/services/Connections.js
@@ -2,9 +2,14 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
+console.log(axios.defaults)
+
 
 export const authService = axios.create({
     baseURL: `${process.env.BACKEND_URL}/auth`,
+    xsrFileds: {
+        withCredentials: true
+    },
     withCredentials: true
 })
 
