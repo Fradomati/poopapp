4bbd94e32ae2051ae4551b314b0641772153a73d
Borja Domínguez <borja.d.atienza@gmail.com>
2020-09-13 20:43:03 +0200

    Problem with Cookie session

--- back/app.js
+++ back/app.js
@@ -64,7 +64,7 @@ app.use(
     resave: true,
     cookie: {
       secure: true,
-      sameSite: true,
+      sameSite: "none",
       httpOnly: false,
       // domain: "127.0.0.1",
       maxAge: 5184000000 // 60 days 
