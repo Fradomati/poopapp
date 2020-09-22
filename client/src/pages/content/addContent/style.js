da2ff7148153bbe3774a8ff7ac15de19a9bdcd82
Borja Domínguez <borja.d.atienza@gmail.com>
2020-09-22 18:26:31 +0200

    Working on content section

---
+++ client/src/pages/content/addContent/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const ContainerAdd = styled.div`
+position: relative;
+    top: 5vh;
+`
+
+export const TextAdded = styled.p`
+width: 90vw;
+margin: 0 auto;
+margin-bottom: 4vh;
+text-align: center;
+`
